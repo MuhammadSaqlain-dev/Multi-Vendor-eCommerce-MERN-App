dd7c723fce5283e841c59f25599007de43852087
Muhammad Saqlain <2mesaqlain@gmail.com>
2023-10-11 02:46:52 +0500

    fix the links

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://multi-vendor-e-commerce-mern-app-frontend.vercel.app",
     credentials: true,
   })
 );
