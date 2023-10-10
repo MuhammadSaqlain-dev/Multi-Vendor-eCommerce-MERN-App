803dc6a2c6abb61250767cf6398bd793ff3f4bd3
Muhammad Saqlain <2mesaqlain@gmail.com>
2023-10-10 15:19:42 +0500

    error handling in email sending

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,31 +1,30 @@
 const nodemailer = require("nodemailer");
+const ErrorHandler = require("./ErrorHandler");
 
 const sendEmail = async (options) => {
-  console.log("sendMail.js");
-  console.log(
-    process.env.SMTP_HOST,
-    process.env.SMTP_PORT,
-    process.env.SMTP_USERNAME,
-    process.env.SMTP_PASSWORD
-  );
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    secure: true,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      secure: true,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.emailMessage,
-  };
+    const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.emailMessage,
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+    return ErrorHandler(500, error.message);
+  }
 };
 
 module.exports = sendEmail;
