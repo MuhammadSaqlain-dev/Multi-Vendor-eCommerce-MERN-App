774d550124254985975073d727b2d0c098875ad7
Muhammad Saqlain <77551019+MuhammadSaqlain-dev@users.noreply.github.com>
2023-10-13 03:17:19 +0500

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -13,6 +13,19 @@ const sendEmail = async (options) => {
       },
     });
 
+    await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
+
     const mailOptions = {
       from: process.env.SMTP_MAIL,
       to: options.email,
@@ -20,7 +33,18 @@ const sendEmail = async (options) => {
       text: options.emailMessage,
     };
 
-    await transporter.sendMail(mailOptions);
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
   } catch (error) {
     console.log(error);
     return ErrorHandler(500, error.message);
